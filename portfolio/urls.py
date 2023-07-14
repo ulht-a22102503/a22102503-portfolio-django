9ba3d6b2d9db4bd3e9fee17d53dc88149d071366
RavingSombra <dafm200936@gmail.com>
2023-07-14 23:41:18 +0100

    Changes still part of the last commit

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -10,5 +10,4 @@ urlpatterns = [
     path('blog/new', views.blog_new_post_view, name='blog_new'),
     path('blog/edit/<int:post_id>', views.blog_edit_post_view, name='blog_edit'),
     path('blog/remove/<int:post_id>', views.blog_remove_post_view, name='blog_remove'),
-    path('random/', views.random_view, name='random'),
 ]
